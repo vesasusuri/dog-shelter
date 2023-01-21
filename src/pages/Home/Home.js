99ae08caa9f3306c89a03ddfe3809639c267d978
Vesa A. Susuri <108492136+vesasusuri@users.noreply.github.com>
2023-01-21 22:24:16 +0100

    banner 3 and banner 4

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,12 +3,14 @@ import Navbar from '../../components/shared/Navbar/Navbar'
 import Banner from '../../components/pages/banner1/Banner'
 import Footer from '../../components/shared/Footer/Footer'
 import Banner2 from '../../components/pages/banner2/banner2'
+import Banner3 from '../../components/pages/banner3/banner3'
 const Home = () => {
     return (
         <div className='home-page'>
             <Navbar />
             <Banner/>
             <Banner2/>
+            <Banner3/>
             <Footer />
         </div>
     )
