34969ad997033f776f105b1db55a0fd0939e100d
Vesa A. Susuri <108492136+vesasusuri@users.noreply.github.com>
2023-01-24 21:53:35 +0100

    last comit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,8 @@ import Footer from '../../components/shared/Footer/Footer'
 import Banner2 from '../../components/pages/banner2/banner2'
 import Banner3 from '../../components/pages/banner3/banner3'
 import Banner4 from '../../components/pages/banner4/banner4'
+import OurServices from '../../components/pages/ourServices/ourServices'
+import Subscribe from '../../components/pages/subscribe/subscribe'
 const Home = () => {
     return (
         <div className='home-page'>
@@ -13,6 +15,8 @@ const Home = () => {
             <Banner2/>
             <Banner3/>
             <Banner4/>
+            <OurServices/>
+            <Subscribe/>
             <Footer />
         </div>
     )
